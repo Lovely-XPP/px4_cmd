9302a8a82f4c62dd05cb9db130f87e03307b4af7
Lovely-XPP <617550202@qq.com>
2023-09-21 00:20:39 +0800

    Update Monitor - Add Generate Python Module

--- template/external_cmd.cpp
+++ template/external_cmd.cpp
@@ -15,7 +15,7 @@ bool detect_px4();
 
 // global var
 QStringList nodes;
-QVector<vehicle_external_command *> data;
+QVector<vehicle_external_command *> uav;
 
 int main(int argc, char *argv[])
 {
@@ -37,21 +37,22 @@ int main(int argc, char *argv[])
         sleep(1);
     }
 
-    // init all agents data
+    // init all agents uav
     for (auto item = nodes.begin(); item != nodes.end(); item++)
     {
         vehicle_external_command *vec = new vehicle_external_command();
         vec->start((*item).toStdString());
-        data.push_back(vec);
+        uav.push_back(vec);
     }
     Info(("External Command Start! Vehicle Count: " + to_string(nodes.size())).c_str());
+    
     /************************ Edit  Here ************************/
     double t = 0;
     while (ros::ok())
     {
         for (size_t i = 0; i < nodes.size(); i++)
         {
-            data[i]->set_position(data[i]->init_x + 2 * sin(0.4 * t), data[i]->init_y + 2 * cos(0.4 * t), 3, px4_cmd::Command::ENU);
+            uav[i]->set_position(uav[i]->init_x + 2 * sin(0.4 * t), uav[i]->init_y + 2 * cos(0.4 * t), 3, px4_cmd::Command::ENU);
         }
         ros::Duration(0.02).sleep();
         t += 0.02;
@@ -59,26 +60,26 @@ int main(int argc, char *argv[])
     return 0;
 
     // Example to get initial position for uavs
-    data[0]->init_x;
-    data[0]->init_y;
-    data[0]->init_z;
+    uav[0]->init_x;
+    uav[0]->init_y;
+    uav[0]->init_z;
 
     // Example to get position (m) / attitude (deg) / velocity (m/s) / angle_rate (deg/s) for uavs
-    data[0]->position[0]; // get x position of uav 0
-    data[0]->velocity[1]; // get y velocity of uav 0
-    data[0]->angle_rate[1]; // get x angle rate of uav 0
-    data[0]->attitude[0]; // get x attitude (Row) of uav 0
-    data[0]->attitude[1]; // get y attitude (Pitch) of uav 0
-    data[0]->attitude[2]; // get z attitude (Yaw) of uav 0
+    uav[0]->position[0]; // get x position of uav 0
+    uav[0]->velocity[1]; // get y velocity of uav 0
+    uav[0]->angle_rate[1]; // get x angle rate of uav 0
+    uav[0]->attitude[0]; // get x attitude (Row) of uav 0
+    uav[0]->attitude[1]; // get y attitude (Pitch) of uav 0
+    uav[0]->attitude[2]; // get z attitude (Yaw) of uav 0
 
     // Example to set desite position / velocity / velocity with height for uavs
     // set desired position to [1, 1, 1] of uav 0, use Body frame (it is recommanded for using global frame: px4_cmd::Command::ENU)
-    data[0]->set_position(1, 1, 1, px4_cmd::Command::BODY);
+    uav[0]->set_position(1, 1, 1, px4_cmd::Command::BODY);
 
     // set desired velocity to [1, 1, 1] of uav 0, use global ENU frame
-    data[0]->set_velocity(1, 1, 1, px4_cmd::Command::ENU);
+    uav[0]->set_velocity(1, 1, 1, px4_cmd::Command::ENU);
     // set desired velocity with height to [1, 1, 1] of uav 0, use global ENU frame 
-    data[0]->set_velocity_with_height(1, 1, 1, px4_cmd::Command::ENU);
+    uav[0]->set_velocity_with_height(1, 1, 1, px4_cmd::Command::ENU);
 
     return 0;
 }
